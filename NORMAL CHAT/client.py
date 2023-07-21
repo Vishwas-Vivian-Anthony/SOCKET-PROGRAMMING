291cd39f8452cdef7be9ada4971f761df3e1dd12
Vishwas-Vivian-Anthony <140191168+Vishwas-Vivian-Anthony@users.noreply.github.com>
2023-07-21 21:56:42 +0530

    client.py

--- NORMAL CHAT/client.py
+++ NORMAL CHAT/client.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import os
 
+# Function to continuously receive messages from the server
 def receive_messages(client_socket):
     while True:
         try:
@@ -10,15 +11,18 @@ def receive_messages(client_socket):
                 break
             print(data)
             if data.lower() == "goodbye":
+                # Close the client socket if the server sends a Goodbye message
                 client_socket.close()
                 break
         except ConnectionResetError:
             # Server disconnected abruptly
             break
 
+# Function to send a file to the server
 def send_file(client_socket, file_path):
     with open(file_path, "rb") as file:
         filename = file_path.split("/")[-1]
+        # Send a message to the server indicating the file transfer
         client_socket.send(("file:" + filename).encode())
         while True:
             file_data = file.read(1024)
@@ -27,6 +31,7 @@ def send_file(client_socket, file_path):
             client_socket.send(file_data)
         print("File '{}' has been sent successfully.".format(filename))
 
+# Main function to initiate the client
 def main():
     host = '127.0.0.1'
     port = 12345
@@ -37,7 +42,7 @@ def main():
     # Connect to the server
     client_socket.connect((host, port))
 
-    # Get the client's name
+    # Get the client's name and register with the server
     client_name = input("Enter your name: ")
     client_socket.send(("REGISTER:" + client_name).encode())
 
@@ -48,8 +53,9 @@ def main():
     while True:
         message = input("Enter message to send ('Bye' to quit): ")
         if message.lower() == "bye":
+            # Send the "Bye" message to the server
             client_socket.send(message.encode())
-            # Wait for the GoodBye message from the server before disconnecting
+            # Wait for the Goodbye message from the server before disconnecting
             receive_thread.join()
             break
         elif message.lower().startswith("/file"):
@@ -60,6 +66,7 @@ def main():
             except FileNotFoundError:
                 print("File not found. Please try again.")
         else:
+            # Send regular chat messages to the server
             client_socket.send(message.encode())
 
 if __name__ == "__main__":
